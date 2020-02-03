1b81a65e1e0395fdfef7d36e45c266945fa3ebfa
alejandroidepaz <adepaz@cs.uoregon.edu>
2020-02-03 15:21:03 -0800

    Added password constraints handler

--- login_forms.py
+++ login_forms.py
@@ -1,13 +1,14 @@
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, Length, Email, ValidationError, EqualTo
 from flask_wtf import FlaskForm
+import re
 
 import mongodb_config as cfg
 from pymongo import MongoClient
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min = 5, max = 35)], render_kw={"placeholder": "Username"})
-    password = PasswordField('Password', validators = [DataRequired(), Length(min = 5, max = 35)], render_kw={"placeholder": "Password"})
+    password = PasswordField('Password', validators = [DataRequired(), Length(min = 8, max = 35)], render_kw={"placeholder": "Password"})
     token = StringField('Token', validators=[DataRequired(), Length(min=6, max=6)], render_kw={"placeholder": "Your 6-Digit Token"})
     submit = SubmitField('Login')
 
@@ -15,7 +16,7 @@ class RegistrationForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired(), Length(min = 5, max = 35)], render_kw={"placeholder": "Username"})
     email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
-    password = PasswordField('Password', validators=[DataRequired(), Length(min = 5, max = 35)], render_kw={"placeholder": "Password"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Password"})
     submit = SubmitField('Register')
 
@@ -32,3 +33,47 @@ class RegistrationForm(FlaskForm):
         user = userdb.user_login_credentials.find_one({"email": email.data})
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+    def validate_password(self, password):
+
+        error_messages = {
+
+            "length": "Minimum Length of 8 | ",
+            "digit": "Minimum 1 Number | ",
+            "uppercase": "Minimum 1 Uppercase Letter | ",
+            "lowercase": "Minimum 1 Lowercase Letter | ",
+            "special_chars": "Minimum 1 Special Character | "
+    
+        }
+        password = password.data
+
+        length_check = len(password) > 7
+
+        digit_check = re.search(r"[0-9]", password) is not None 
+
+        uppercase_check = re.search(r"[A-Z]", password) is not None
+
+        lowercase_check = re.search(r"[a-z]", password) is not None
+
+        special_chars_check = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is not None
+
+        password_verified = length_check and digit_check and uppercase_check and lowercase_check and special_chars_check
+
+        results_dict = {
+
+            "length": length_check,
+            "digit": digit_check,
+            "uppercase": uppercase_check,
+            "lowercase": lowercase_check,
+            "special_chars": special_chars_check
+            
+        }
+
+        if not password_verified:
+            error_message = ""
+            for key in results_dict.keys():
+                print(f'{key}: {results_dict[key]}')
+                if not results_dict[key]:
+                    error_message += error_messages[key]
+            
+            raise ValidationError("Password Failed the following criteria:\n" + error_message)
